1a960af3e40670e29782f21e0ca38398125f87c0
hi991016 <miureus@gmail.com>
2025-07-29 12:28:23 +0700

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// ===== globals =====
+const isMobile = window.matchMedia("(max-width: 1024px)");
+const eventsTrigger = ["pageshow", "scroll"];
+
+// ===== init =====
+const init = () => {
+  // # app height
+  appHeight();
+  // # lazy load
+  const ll = new LazyLoad({
+    threshold: 0,
+    elements_selector: ".lazy",
+  });
+};
+
+// ===== app height =====
+const appHeight = () => {
+  const doc = document.documentElement;
+  const menuH = Math.max(doc.clientHeight, window.innerHeight || 0);
+
+  if (!isMobile.matches) return;
+
+  doc.style.setProperty("--app-height", `${doc.clientHeight}px`);
+  doc.style.setProperty("--menu-height", `${menuH}px`);
+};
+window.addEventListener("resize", appHeight);
+
+// ### ===== DOMCONTENTLOADED ===== ###
+window.addEventListener("DOMContentLoaded", init);
